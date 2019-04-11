b1c4e93e061bbde6901c5e4dfa1316ab4a99c2e7
spm176 <be-felix@web.de>
2019-04-11 09:24:39 +0200

    Delete unused files

--- ur_cut/scripts/Distance_search.py
+++ ur_cut/scripts/Distance_search.py
@@ -9,21 +9,21 @@ from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 
 
-
-
 def startCutting():
     rospy.loginfo('Package found: Start Cutting!') 
-    #time.sleep(3)
+    #time.sleep(3) # some delay to wait package all the way
     # TODO aufrufen von cutter
 
 def searchForDistanceIn(point_cloud):
-    points_3d = []
     
+    # Distance is not searched for all points, only in the middle of the image
+    # Create 2D Array with points to search distance for
     midpoint_x = (95,96,97,98,99,100,101,102,103,104)
     midpoint_y = (80,81,82,83,84,85,86,87,88,89)
     
+    rospy.loginfo(" ")
     rospy.loginfo("Get distance from:")
-    rospy.loginfo("X: " + str(midpoint_x) + "Y: " + str(midpoint_y))
+    #rospy.loginfo("X: " + str(midpoint_x) + "Y: " + str(midpoint_y))
 
 
     for mx in midpoint_x:
@@ -31,7 +31,7 @@ def searchForDistanceIn(point_cloud):
             midpoint = (mx, my)
             points_2d = ([midpoint])
 
-            # skip_nans to protect for traceback NaN Error
+            # skip_nans is needed to protect for traceback NaN Error
             for point_3d in list(pc2.read_points(point_cloud, uvs=points_2d, skip_nans=True)):
                 x, y, z = point_3d
                 return z
@@ -43,23 +43,35 @@ def packageInDistance(point_cloud):
     z = searchForDistanceIn(point_cloud)
     if z is not None:
         rospy.loginfo("Distanze to Object: " + str(z))
-        if z < 0.15:
-            rospy.loginfo("Close Object, is in cutting!")
+
+        if z < 0.10:
+            rospy.loginfo("WARNING: Should not be possible!")
+            return False
         elif 0.15 < z < 0.35:
-            rospy.loginfo("Packet in Range! Start cutting ...")
+            rospy.loginfo("Package in Range! Start cutting ...")
             return True
+        elif z > 0.35:
+            rospy.loginfo("No Package in Range")
+            return False
 
     rospy.loginfo("No Distance in PointCloud")
     return False
 
 def callback(msg):  
+    global sub_Node
     if packageInDistance(msg):
+        # unsubscribe node, messages not needed while cutting
+        sub_Node.unregister()
         startCutting()
+        sub_Node = rospy.Subscriber("/royale_camera_driver/point_cloud", PointCloud2, callback)
+
 
 
 def main():
+    global sub_Node
+
     rospy.init_node('packet_cut', anonymous=True, disable_signals=True)
-    rospy.Subscriber("/royale_camera_driver/point_cloud", PointCloud2, callback)
+    sub_Node = rospy.Subscriber("/royale_camera_driver/point_cloud", PointCloud2, callback)
 
     try:
         rospy.spin()
@@ -69,3 +81,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
